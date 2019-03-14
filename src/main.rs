0de0b90b2c9d646472f10c302127471095fcc405
Sindre Bøyum <sindre@acos.no>
2019-03-14 10:27:53 +0100

    Task 1 completed

--- src/main.rs
+++ src/main.rs
@@ -34,9 +34,39 @@ fn main() -> Result<(), Error> {
 
             println!("{:?}", action);
         }
+
+        let frequency: f64;
+        let get_note = move |note: i32| {
+            440_f64 * 1.059463094359_f64.powi(note)
+        };
+
+        match action {
+            Some(i) => frequency = get_note(i),
+            None => frequency = 0.0
+        }
         
-        // TODO: Implement your synthesizer here
-        0.0
+        let amplitude: f64 = 1.0;
+        let phase: f64 = 1.0;
+        let pi = PI;
+
+        let sine = f64::from(2) * PI * frequency * time + phase;
+        let sine_wave = amplitude * sine.sin();
+
+        let clamp = move |min: f64, max: f64, num: f64| {
+            let ret: f64;
+            
+            if num < min {
+                ret = min;
+            } else if num > max {
+                ret = max;
+            } else {
+                ret = num
+            }
+
+            ret
+        };
+
+        clamp(0.01_f64, 0.99_f64, sine_wave)
     };
 
     audioengine.set_processor_function(Box::new(synth));
